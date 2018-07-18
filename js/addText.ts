1c256e4080505c0d2a0e351ee7b00ed781c767ee
qebsler <dguillet6@gmail.com>
2018-07-18 14:28:35 +0200

    add files

---
+++ js/addText.ts
@@ -0,0 +1,5 @@
+function addText (text: string, element: HTMLElement):void {
+   element.innerText = text
+}
+
+export default addText
---
+++ js/index.ts
@@ -0,0 +1,4 @@
+import addTextToTextarea from './addText'
+import textMarkdown from './markdown'
+
+addTextToTextarea(textMarkdown, document.getElementById('markdown-input'))
